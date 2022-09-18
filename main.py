1d73bb6f7a5af9e67c866aa07fc2e4f4c6586a14
Sergey Chukin <chuk2208@yandex.ru>
2022-09-18 16:21:47 +0500

    Исправления

--- main.py
+++ main.py
@@ -34,16 +34,16 @@ def quicksort(l, r, nums):
 
 
 def print_matrix(example):
-	print(quicksort(0, len(example)-1, example))
+	print(example)
 
 
 example = [4, 5, 1, 2, 3]
 result = [1, 2, 3, 4, 5]
-print_matrix(example)
+print_matrix(quicksort(0, len(example)-1, example))
 print_matrix(result)
 
 example = [2, 5, 6, 1, 4, 6, 2, 4, 7, 8]
 result = [1, 2, 2, 4, 4, 5, 6, 6, 7, 8]
 # As you can see, it works for duplicates too
-print_matrix(example)
+print_matrix(quicksort(0, len(example)-1, example))
 print_matrix(result)
